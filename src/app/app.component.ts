c2ad0ae39ac5fb8e9ef46f91b621b1b59515b266
Sreenadh T C <kesav.tc8@gmail.com>
2017-07-04 14:13:13 +0530

    Add Navbar components, use Bootswatch theme: 	new file:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/components/navbar/navbar.component.html 	new file:   src/app/components/navbar/navbar.component.ts 	modified:   src/index.html 	new file:   src/static/css/bootstrap.min.css 	deleted:    src/styles.css 	new file:   yarn.lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+	moduleId:module.id,
+	selector: 'my-app',
+	templateUrl: `app.component.html`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { }
